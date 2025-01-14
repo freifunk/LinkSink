c5f6af29d0a01b49511faf32dc6229cfdebef9d5
Andreas Bräu <ab@andi95.de>
2025-01-15 00:09:07 +0100

    update envtest

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -12,3 +12,6 @@ $env = $_SERVER['APP_ENV'] ?? 'dev';
 if ($env === 'prod' && file_exists(dirname(__DIR__).'/.env.production')) {
     (new Dotenv())->usePutenv()->overload(dirname(__DIR__).'/.env.production');
 }
+if ($env === 'test' && file_exists(dirname(__DIR__).'/.env.test')) {
+    (new Dotenv())->usePutenv()->overload(dirname(__DIR__).'/.env.test');
+}
